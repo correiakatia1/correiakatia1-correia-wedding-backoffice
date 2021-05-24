6e23f0959d7ecaf6aa10942b07bf87665ffd063c
kcorreia <correia.katia1@gmail.com>
2021-05-24 16:27:09 +0100

    test

--- src/Controller/DressController.php
+++ src/Controller/DressController.php
@@ -9,12 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class DressController extends AbstractController
 {
     /**
-     * @Route("/dress", name="dress")
+     * @Route("/dress", name="dress_list")
      */
     public function index(): Response
     {
-        return $this->render('dress/index.html.twig', [
-            'controller_name' => 'DressController',
-        ]);
+        return $this->render('dress/list.html.twig');
     }
 }
