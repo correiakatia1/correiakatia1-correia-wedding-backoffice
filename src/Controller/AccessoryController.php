0b07d5c93a960566b5a21a870ef7d57d16e63c49
kcorreia <correia.katia1@gmail.com>
2021-06-16 22:49:24 +0100

    accessory(new) accessory(emigration,tabela categoria)

--- src/Controller/AccessoryController.php
+++ src/Controller/AccessoryController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Accessory;
+use App\Repository\AccessoryCategoryRepository;
+use App\Repository\ColorRepository;
+use App\Repository\DetailRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +13,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccessoryController extends AbstractController
 {
     /**
+     * @var AccessoryCategoryRepository
+     */
+    private $accessoryCategoryRepository;
+    /**
+     * @var DetailRepository
+     */
+    private $detailRepository;
+    /**
+     * @var ColorRepository
+     */
+    private $colorRepository;
+
+
+
+    public function __construct(
+        AccessoryCategoryRepository $AccessoryCategoryRepository,
+        DetailRepository $DetailRepository,
+        ColorRepository $colorRepository
+    )
+    {
+        $this->accessoryCategoryRepository = $AccessoryCategoryRepository;
+        $this->detailRepository = $DetailRepository;
+        $this->colorRepository = $colorRepository;
+    }
+
+    /**
      * @Route("/accessories", name="accessory_list")
      */
-    public function index(): Response{
+    public function index(): Response
+    {
         return $this->render('accessory/list.html.twig');
     }
 
@@ -21,9 +51,19 @@ class AccessoryController extends AbstractController
      */
     public function new(): Response
     {
+        $details = $this->detailRepository->findAll();
+        $colors = $this->colorRepository->findAll();
+        $categories = $this->accessoryCategoryRepository->findAll();
+
+
+
+
         $accessory = new Accessory();
         return $this->render('accessory/new.html.twig', [
-            'accessory' => $accessory
+            'accessory' => $accessory,
+            'details' => $details,
+            'colors' => $colors,
+            'accessoryCategories'=>$categories,
         ]);
     }
 }
